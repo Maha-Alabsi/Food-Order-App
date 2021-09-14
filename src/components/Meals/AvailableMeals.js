c76b3b81a0ca9d5166bdb89cc025c634ffc660f8
Maha-Alabsi <mahaahalabsi@gmail.com>
2021-09-14 13:36:41 +0300

    add card & mealItem

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,22 @@
 import classes from './AvailableMeals.module.css';
-import DUMMY_MEALS from './../../fixtures/DUMMY_MEALS.js'
+import DUMMY_MEALS from './../../fixtures/DUMMY_MEALS.js';
+import MealItem from './MealItem/MealItem';
+import Card from '../UI/Card';
+
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map(meal => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
